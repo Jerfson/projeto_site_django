64b9fbcf026e313c851ca7fbcdb8c959e36d62ce
Seu Nome <jerfson.r@gmail.com>
2026-05-13 13:15:49 -0300

    Allow all hosts in settings

--- reboques_site/settings.py
+++ reboques_site/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'catalogo',
